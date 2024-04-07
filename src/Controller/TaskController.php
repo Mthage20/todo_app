939605768914380d570c40bbd6c384e575215863
Mthage20 <90241683+Mthage20@users.noreply.github.com>
2024-04-07 17:20:38 +0200

    Initial setup with controller done
    initial setup of the project + the task controller is done.

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class TaskController extends AbstractController
+{
+    #[Route('/task', name: 'app_task')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/TaskController.php',
+        ]);
+    }
+}
