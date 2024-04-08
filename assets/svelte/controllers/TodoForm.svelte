60f27bcf1dec21b02ab69b535b8b42466e1f029e
Mthage20 <90241683+Mthage20@users.noreply.github.com>
2024-04-08 02:53:40 +0200

    Minor changes to twig and svelte files

--- assets/svelte/controllers/TodoForm.svelte
+++ assets/svelte/controllers/TodoForm.svelte
@@ -1,8 +1,8 @@
 <script>
-  let title;
-  let description;
+let title;
+let description;
 </script>
-<h1> Hej</h1>
+
 <form>
   <input type="text" placeholder="title" bind:value={title}>
   <input type="text" placeholder="description" bind:value={description}>
